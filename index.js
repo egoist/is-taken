32bebb68b3818b7967031314682ed18f8c88393e
EGOIST <0x142857@gmail.com>
2016-02-21 23:04:02 +0800

    add opts

--- index.js
+++ index.js
@@ -3,13 +3,16 @@
 var fetch = require('isomorphic-fetch')
 var Promise = require('es6-promise').Promise
 
-module.exports = function (name) {
+module.exports = function (name, opts) {
 	if (typeof name !== 'string') {
 		return Promise.reject(new TypeError('Expected a string'))
 	}
 
-	var registry = process.env.NPM_REGISTRY_URL || 'https://registry.npmjs.org/'
-	return fetch(registry + name)
+	opts = opts || {}
+	opts.registry = opts.registry || process.env.NPM_REGISTRY_URL || 'https://registry.npmjs.org/'
+	opts.timeout = opts.timeout || 2000
+
+	return fetch(opts.registry + name, {timeout: opts.timeout})
 		.then(function (data) {
 			if (data.status === 404) {
 				return false
